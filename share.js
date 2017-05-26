5437d99f1d50680c18fe6c8d6e1da4866de90b06
CoderSeoClub <doc.viktor@gmail.com>
2017-05-26 14:28:43 +0300

    Created
    Created

---
+++ share.js
@@ -0,0 +1,41 @@
+<div class="ya-share2__container ya-share2__container_size_m"><ul class="ya-share2__list ya-share2__list_direction_horizontal">
+
+
+vkontakte
+ВКонтакте
+https://vk.com/share.php?url=[url]&title=[title]&description=[]&image=[]
+
+facebook
+https://www.facebook.com/sharer.php?src=sp&amp;u=[]&amp;title=[]&description=[]&picture=[]
+
+odnoklassniki
+https://connect.ok.ru/offer?url=[]&amp;title=[title]&amp;description=&amp;imageUrl=
+
+moimir
+https://connect.mail.ru/share?url=[url]&amp;title=[title]&amp;description=
+
+gplus
+https://plus.google.com/share?url=[url]
+
+twitter
+https://twitter.com/intent/tweet?text=[title]&amp;url=[url]
+
+
+
+
+
+
+<li class="ya-share2__item ya-share2__item_service_[name]"><a class="ya-share2__link" href="[href]" rel="nofollow" target="_blank" title="[ru-name]"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">[ru-name]</span></a></li>
+
+<li class="ya-share2__item ya-share2__item_service_facebook"><a class="ya-share2__link" href="https://www.facebook.com/sharer.php?src=sp&amp;u=h[url]&amp;title=[title]&amp;description=&amp;picture=&amp;utm_source=share2" rel="nofollow" target="_blank" title="Facebook"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">Facebook</span></a></li>
+
+<li class="ya-share2__item ya-share2__item_service_odnoklassniki"><a class="ya-share2__link" href="https://connect.ok.ru/offer?url=h[url]&amp;title=[title]&amp;description=&amp;imageUrl=&amp;utm_source=share2" rel="nofollow" target="_blank" title="Одноклассники"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">Одноклассники</span></a></li>
+
+<li class="ya-share2__item ya-share2__item_service_moimir"><a class="ya-share2__link" href="https://connect.mail.ru/share?url=h[url]&amp;title=[title]&amp;description=&amp;utm_source=share2" rel="nofollow" target="_blank" title="Мой Мир"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">Мой Мир</span></a></li>
+
+<li class="ya-share2__item ya-share2__item_service_gplus"><a class="ya-share2__link" href="https://plus.google.com/share?url=h[url]&amp;utm_source=share2" rel="nofollow" target="_blank" title="Google+"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">Google+</span></a></li>
+
+
+<li class="ya-share2__item ya-share2__item_service_twitter"><a class="ya-share2__link" href="https://twitter.com/intent/tweet?text=[title]&amp;url=h[url]&amp;hashtags=&amp;utm_source=share2" rel="nofollow" target="_blank" title="Twitter"><span class="ya-share2__badge"><span class="ya-share2__icon"></span></span><span class="ya-share2__title">Twitter</span></a></li>
+
+</ul></div>
